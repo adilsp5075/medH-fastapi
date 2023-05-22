a5a95ea861ace742dfef73e6d7ca5c4633d26f82
v4zha <arvin_j4848@protonmail.com>
2023-05-22 11:33:11 +0530

    smtp sendinblue

--- utils/email.py
+++ utils/email.py
@@ -9,14 +9,13 @@ load_dotenv()
 def send_email(receiver_email, subject, body):
     sender_email = os.environ.get("SENDER_EMAIL")
     sender_password = os.environ.get("SENDER_PASSWORD")
-
     message = MIMEMultipart()
     message["From"] = sender_email
     message["To"] = receiver_email
     message["Subject"] = subject
     message.attach(MIMEText(body, "plain"))
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    with smtplib.SMTP("smtp-relay.sendinblue.com", 587) as server:
         server.starttls()
         server.login(sender_email, sender_password)
         server.sendmail(sender_email, receiver_email, message.as_string())
